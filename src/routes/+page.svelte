da01ac5a2cd753bf4409454c7c87f60eb3e4eb0b
Codrin Mares <codrin.mares@gmail.com>
2023-11-23 18:25:26 +0200

    Test commit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  console.log('Im in');
+</script>
+
 <h1 class="text-green-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
